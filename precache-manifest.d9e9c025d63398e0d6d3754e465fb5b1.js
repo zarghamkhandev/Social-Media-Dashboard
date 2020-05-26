d54b0f75ce38c737eab41501c4546e3da6cb6cbf
unknown <zarghamkhan980@gmail.com>
2020-05-27 00:32:07 +0200

    Updates

---
+++ precache-manifest.d9e9c025d63398e0d6d3754e465fb5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1688903fdde2f9b0eaad5fbe144186",
+    "url": "/Social-Media-Dashboard/index.html"
+  },
+  {
+    "revision": "4e9a21a91835152990d0",
+    "url": "/Social-Media-Dashboard/static/css/main.c062a729.chunk.css"
+  },
+  {
+    "revision": "90691edd2be8dec927d3",
+    "url": "/Social-Media-Dashboard/static/js/2.02b07c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Social-Media-Dashboard/static/js/2.02b07c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9a21a91835152990d0",
+    "url": "/Social-Media-Dashboard/static/js/main.5f3f0a54.chunk.js"
+  },
+  {
+    "revision": "66011aec3f3e077345d5",
+    "url": "/Social-Media-Dashboard/static/js/runtime-main.b94d5a2b.js"
+  }
+]);
